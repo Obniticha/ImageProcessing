170de4388a7632f4335d245ef004ac51216963fb
Obniticha <41154121+Obniticha@users.noreply.github.com>
2018-12-24 23:57:51 +0700

    Add files via upload

---
+++ Morphological_filter.m
@@ -0,0 +1,23 @@
+clear; clc;
+I_jpg = imread('image1.jpg');
+BW = im2bw(I_jpg,0.5);
+%% open and close
+SE2 = strel('rectangle',[20 20]);
+BW2 = imopen(BW, SE2);
+SE3 = strel('rectangle',[10 5]);
+BW3 = imclose(BW,SE3);
+
+figure
+subplot(1,3,1),imshow(BW)
+subplot(1,3,2),imshow(BW2)
+subplot(1,3,3),imshow(BW3)
+%% erode and dilate
+SE4 = strel('rectangle',[30 20]);
+BW4 = imerode(BW,SE4);
+SE5 = strel('rectangle',[20 10]);
+BW5 = imdilate(BW,SE5);
+
+figure
+subplot(1,3,1),imshow(BW)
+subplot(1,3,2),imshow(BW4)
+subplot(1,3,3),imshow(BW5)
